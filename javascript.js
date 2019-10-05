a5a6c1e1d61eef7033ea80246fd32efce9131414
Efren_Marin <efren45marin@gmail.com>
2019-10-05 08:06:20 -0700

    Finished styling the password generator. Aligned items for aesthetic purposes. Colored buttons and linked JS external file to html doc.

---
+++ javascript.js
@@ -0,0 +1,19 @@
+let specialChar = document.getElementById("#pw-requirement1");
+let numChar = document.getElementById("#pw-requirement2");
+let lowChar = document.getElementById("#pw-requirement3");
+let upChar = document.getElementById("#pw-requirement4");
+let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+let generatePass = document.getElementById("#generate");
+let copyPass = document.getElementById("#copy");
+let stringLength = 8;
+
+
+console.log(specialChar);
+console.log(numChar);
+console.log(lowChar);
+console.log(upChar);
+console.log(chars);
+console.log(generatePass);
+console.log(copyPass);
+console.log(stringLength);
+
