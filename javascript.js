999c676cf0a176c40607a9ba659eecad5f8d413e
Efren_Marin <efren45marin@gmail.com>
2020-01-20 22:44:57 -0800

    Removed legacy code and started fresh with the js file.

--- javascript.js
+++ javascript.js
@@ -1,83 +1,10 @@
+var passField = document.querySelector("#text-field");
+var generatePass = document.querySelector("#generate");
 
-//Define variables
-// let greeting = alert("Welcome to the Password Generator!");
-// let passLength = prompt ("How long would you like your password to be? For security, please choose from 8 - 128 characters long. (Use only numerical values)");
-// let userNumChar = prompt ("Would you like to have numbers in your password? Use Y or N");
-// let userLowChar = prompt ("Would you like to have lowercase characters in your password? Use Y or N");
-// let userUpChar = prompt ("Would you like to have uppercase characters in your password? Use Y or N");
-// let userSpecialChar = prompt ("Would you like to have special characters in your password? Use Y or N");
-var userPass = document.querySelector("#password");
-let generatePass = document.querySelector("#generate");
-let copyPass = document.querySelector("#copy");
-let password = "";
+generatePass.addEventListener('click', function(){
+    var startPass = document.getElementById('text-field');
+    startPass.setAttribute('class', 'hide');
 
-
-//Array of paramenters
-var numbers = '1234567890';
-var lowChar = 'abcdefghijklmnopqrstuvwxyz';
-var upChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var specialChar = '!@#$%^&*';
-var selectedPass = [];
-
-// Testing to see that variables are successful
-console.log(passLength);
-console.log(userSpecialChar);
-console.log(userNumChar);
-console.log(userLowChar);
-console.log(userUpChar);
-
-
-function startPass (){
-    var passArea = document.getElementById("#password");
-    passArea.innerHTML = "How long would you like your password to be? For security, please choose from 8 - 128 characters long.";
-    
-    var userSelect = document.createElement('') 
-}
-
-
-
-
-generatePass.addEventListener("click", function(){
-    var password = "";
-    var numChar = (1234567890);
-    var numCharLength = numChar.length;
-    for (var i = 0; i < numChar.length; i++) {
-        password += numChar.charAt(Math.floor(Math.random() * numCharLength));
-    }
-    return this.password;
-    }
-);    
-generatePass.addEventListener("click", function(){
-    var password = "";
-    var lowChar = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-    var lowCharLength = lowChar.length;
-    for (var i = 0; i < lowChar.length; i++) {
-        password += lowChar.charAt(Math.floor(Math.random() * lowCharLength));
-    }
-    return password;
-}
-);
-generatePass.addEventListener("click", function(){
-    var password = "";
-    var upChar = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
-    var upCharLength = upChar.length;
-    for (var i = 0; i < upChar.length; i++) {
-        password += upChar.charAt(Math.floor(Math.random() * upCharLength));
-}
-return password;
-}
-);
-generatePass.addEventListener("click", function(){
-    var password = "";
-    var specialChar = ('!','@','#','$','%','^','&','*');
-    var specialCharLength = specialChar.length;
-    for (var i = 0; i < specialChar.length; i++) {
-        password += specialChar.charAt(Math.floor(Math.random() * specialCharLength));
-}
-return this.password;
-}
-);
-console.log(this.password);
-
-
-
+    var hideButtons = document.getElementById('buttons');
+    hideButtons.setAttribute('class', 'hide');
+})
