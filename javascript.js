d0869b95be5109f281a1fa11dd39b4e528806a6c
Efren_Marin <efren45marin@gmail.com>
2020-01-20 23:44:47 -0800

    Added styling and worked on new JS file.

--- javascript.js
+++ javascript.js
@@ -2,9 +2,29 @@ var passField = document.querySelector("#text-field");
 var generatePass = document.querySelector("#generate");
 
 generatePass.addEventListener('click', function(){
-    var startPass = document.getElementById('text-field');
-    startPass.setAttribute('class', 'hide');
+    // var startPass = document.getElementById('text-field');
+    // startPass.setAttribute('class', 'hide');
 
     var hideButtons = document.getElementById('buttons');
     hideButtons.setAttribute('class', 'hide');
+
+    passLength();
 })
+
+function passLength(){
+    document.getElementById('text-field').innerText = 'Please choose a password length between 1-128 characters';
+    var passLengthInput = document.createElement('textarea');
+    var nextBtn = document.createElement('button');
+
+    passLengthInput.name = 'length';
+    passLengthInput.maxLength = '3';
+    passLengthInput.cols = '5';
+    passLengthInput.rows = '5';
+
+    passLengthInput.setAttribute('id', 'passLengthInput');
+    nextBtn.setAttribute('id','nextBtn');
+    nextBtn.innerHTML = 'Next';
+
+    passField.appendChild(passLengthInput);
+    passField.appendChild(nextBtn);
+}
