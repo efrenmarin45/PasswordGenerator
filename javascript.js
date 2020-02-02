2b16f97050ea3b3f3eb333aabb011c962d7acfbc
Efren_Marin <efren45marin@gmail.com>
2020-02-01 22:42:30 -0800

    Added more functions to help capture user responses.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
 // Variables
-var passField = document.querySelector("#text-field");
-var generatePass = document.querySelector("#generate");
+let passField = document.querySelector("#text-field");
+let generatePass = document.querySelector("#generate");
 
 
 //Array to hold user input
-var passComponents = [];
+let passComponents = [];
 
 
 generatePass.addEventListener('click', function(){
-    var hideButtons = document.getElementById('buttons');
+    let hideButtons = document.getElementById('buttons');
     hideButtons.setAttribute('class', 'hide');
 
     passLength();
@@ -16,8 +16,8 @@ generatePass.addEventListener('click', function(){
 
 function passLength(){
     document.getElementById('text-field').innerText = 'Please choose a password length between 1-128 characters';
-    var passLengthInput = document.createElement('textarea');
-    var nextBtn = document.createElement('button');
+    let passLengthInput = document.createElement('textarea');
+    let nextBtn = document.createElement('button');
 
     passLengthInput.name = 'length';
     passLengthInput.maxLength = '3';
@@ -32,9 +32,94 @@ function passLength(){
     passField.appendChild(nextBtn);
 
     document.getElementById('nextBtn').addEventListener('click', function(){
-        var userLength = passLengthInput.value;
+        let userLength = passLengthInput.value;
         passComponents.push(userLength);
         console.log(passComponents);
+
+        specialPass();
     })
 }
 
+function specialPass(){
+
+    //Asking user if they would like Special Characters in their password
+    document.getElementById('text-field').innerText = 'Would you like to use special characters?';
+    let nextBtn = document.createElement('button');
+
+    //Yes Radio Option
+    let passSpecialInput = document.createElement('input');
+    passSpecialInput.setAttribute('type', 'radio');
+    passSpecialInput.setAttribute('id', 'passSpecialYes');
+    passSpecialInput.setAttribute('value', 'Yes');
+    passSpecialInput.setAttribute('name', 'choice');
+    let passSpecialLabel = document.createElement('label');
+    passSpecialLabel.innerHTML = 'Yes';
+    passField.appendChild(passSpecialLabel);
+    passField.appendChild(passSpecialInput);
+
+    //No Radio Option
+    let passSpecialInputNo = document.createElement('input');
+    passSpecialInputNo.setAttribute('type', 'radio');
+    passSpecialInputNo.setAttribute('id', 'passSpecialNo');
+    passSpecialInputNo.setAttribute('value', 'No');
+    passSpecialInputNo.setAttribute('name', 'choice');
+    let passSpecialLabelNo = document.createElement('label');
+    passSpecialLabelNo.innerHTML = 'No';
+    passField.appendChild(passSpecialLabelNo);
+    passField.appendChild(passSpecialInputNo);
+
+
+    nextBtn.setAttribute('id','nextBtn');
+    nextBtn.innerHTML = 'Next';
+
+    passField.appendChild(nextBtn);
+
+    document.getElementById('nextBtn').addEventListener('click', function(){
+        let userSpecial = document.querySelector('input[name="choice"]:checked').value;
+        passComponents.push(userSpecial);
+        console.log(passComponents);
+
+        numberPass();
+    })
+}
+
+
+function numberPass(){
+
+    //Asking user if they would like numbers in their password
+    document.getElementById('text-field').innerText = 'Would you like to use numbers in your password?';
+    let nextBtn = document.createElement('button');
+
+    //Yes Radio Option
+    let passNumInput = document.createElement('input');
+    passNumInput.setAttribute('type', 'radio');
+    passNumInput.setAttribute('id', 'passSpecialYes');
+    passNumInput.setAttribute('value', 'Yes');
+    passNumInput.setAttribute('name', 'choice');
+    let passNumLabel = document.createElement('label');
+    passNumLabel.innerHTML = 'Yes';
+    passField.appendChild(passNumLabel);
+    passField.appendChild(passNumInput);
+
+    //No Radio Option
+    let passNumInputNo = document.createElement('input');
+    passNumInputNo.setAttribute('type', 'radio');
+    passNumInputNo.setAttribute('id', 'passSpecialNo');
+    passNumInputNo.setAttribute('value', 'No');
+    passNumInputNo.setAttribute('name', 'choice');
+    let passNumLabelNo = document.createElement('label');
+    passNumLabelNo.innerHTML = 'No';
+    passField.appendChild(passNumLabelNo);
+    passField.appendChild(passNumInputNo);
+
+    nextBtn.setAttribute('id','nextBtn');
+    nextBtn.innerHTML = 'Next';
+
+    passField.appendChild(nextBtn);
+
+    document.getElementById('nextBtn').addEventListener('click', function(){
+        let userNum = document.querySelector('input[name="choice"]:checked').value;
+        passComponents.push(userNum);
+        console.log(passComponents);
+    })
+}
